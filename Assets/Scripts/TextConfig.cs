e86d194b36e87facc21438658c8be9b09fc36903
Dorian <cichoszewski.dorian@gmail.com>
2025-11-19 13:26:16 +0100

    JsonUtility Attempt

---
+++ Assets/Scripts/TextConfig.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace QDC.Text
+{
+    // Custom Editor: TextConfigEditor
+    [CreateAssetMenu(menuName = "QDC/TextConfig")]
+    public class TextConfig : ScriptableObject
+    {
+        [SerializeField] private string jsonData;
+
+        public string JsonData
+        {
+            get { return jsonData; }
+            set { jsonData = value; }
+        }
+
+        public void LoadFrom(TextMeshProUGUI source)
+        {
+            // Remove text temporarily, so that it won't be stored in config
+            var originalText = source.text;
+            source.text = string.Empty;
+            jsonData = JsonUtility.ToJson(source, true);
+            source.text = originalText;
+        }
+
+        public void ApplyTo(TextMeshProUGUI target)
+        {
+            
+            if (target == null) return;
+            var originalText = target.text;
+            JsonUtility.FromJsonOverwrite(jsonData, target);
+            target.text = originalText;
+        }
+    }
+}
--- Assets/Scripts/TextWrapper.cs
+++ Assets/Scripts/TextWrapper.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -10,36 +9,91 @@ namespace QDC.Text
     public class TextWrapper : MonoBehaviour
     {
         [SerializeField] private string textTest;
+
+        [SerializeField] private TextConfig textConfig;
         
         private TextMeshProUGUI tmpText;
 
         public TextMeshProUGUI TMPText => tmpText;
+        
+        public bool IsDataReady => textConfig != null;
 
         private void Awake()
         {
-            if (tmpText != null) return;
+            CreateUnderlyingText();
+        }
+
+        private void OnEnable()
+        {
+            tmpText.enabled = true;
+        }
+
+        private void OnDisable()
+        {
+            tmpText.enabled = false;
+        }
+
+        private void OnValidate()
+        {
+            // If data is wrong, disable (hide) underlying Text
+            if (tmpText && !IsDataReady)
+                tmpText.enabled = false;
             
-            var tmpGameObject = new GameObject(gameObject.name + "_TMP", typeof(RectTransform));
+            // Do nothing if Data isn't ready
+            if (!IsDataReady)
+                return;
+            
+            if (!tmpText)
+            {
+                Debug.LogWarning($"Restoring text for wrapper {name}.\nUnderlying text shouldn't be removed individually.", gameObject);
+                CreateUnderlyingText();
+            }
+
+            tmpText.enabled = enabled;
+            
+            textConfig.ApplyTo(tmpText);
+            
+            if (textTest != tmpText.text)
+            {
+                tmpText.text = textTest;
+            }
+        }
+        
+        private void OnDestroy()
+        {
+            DestroyText();
+        }
+
+        private void CreateUnderlyingText()
+        {
+            // Don't have to do anything
+            if (tmpText) return;
+
+            // Can't initialize if data isn't ready
+            if (!IsDataReady)
+            {
+                Debug.LogError("Trying to show text without correct data", gameObject);
+                return;
+            }
+            
+            var tmpGameObject = new GameObject(gameObject.name + "_TMP", typeof(RectTransform), typeof(TextMeshProUGUI))
+            {
+#if SHOW_WRAPPED_TEXT
+                hideFlags = HideFlags.DontSave | HideFlags.NotEditable
+#else
+                hideFlags = HideFlags.DontSave | HideFlags.NotEditable | HideFlags.HideInHierarchy
+#endif
+            };
             var rectTransform = tmpGameObject.GetComponent<RectTransform>();
             GameObjectUtility.SetParentAndAlign(tmpGameObject, gameObject);
             rectTransform.SetAsFirstSibling();
             rectTransform.anchorMax = Vector2.one;
             rectTransform.anchorMin = Vector2.zero;
             rectTransform.sizeDelta = Vector2.zero;
-            tmpGameObject.hideFlags = HideFlags.DontSave;
-            tmpText = tmpGameObject.AddComponent<TextMeshProUGUI>();
+            tmpText = tmpGameObject.GetComponent<TextMeshProUGUI>();
         }
 
-        private void OnValidate()
-        {
-            if (tmpText == null) return;
-            if (textTest != tmpText.text)
-            {
-                tmpText.text = textTest;
-            }
-        }
-
-        private void OnDestroy()
+        public void DestroyText()
         {
             // Destroy text if it exists
             try
